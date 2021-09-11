511c593d3994214814a39f0a68237a35bc2af0fd
jaeheonYoo325 <yoojh0325@gmail.com>
2021-09-12 01:56:49 +0900

    2021.09.12 : 요구사항 분석과 기본 매핑.

---
+++ src/main/java/com/jpabasic/jpashop/domain/Order.java
@@ -0,0 +1,28 @@
+package com.jpabasic.jpashop.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+	
+	@Id @GeneratedValue
+	@Column(name = "ORDER_ID")
+	private Long id;
+	
+	@Column(name = "MEMBER_ID")
+	private Long memberId;
+	
+	private LocalDateTime orderDate; //ORDER_DATE, order_date
+	
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+}
---
+++ src/main/java/com/jpabasic/jpashop/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.jpabasic.jpashop.domain;
+
+public enum OrderStatus {
+	ORDER, CANCEL
+}
