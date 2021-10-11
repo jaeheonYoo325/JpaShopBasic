10a85d20eba0564b27cd59cdeae3b7bd6b3e49c2
jaeheonYoo325 <yoojh0325@gmail.com>
2021-10-11 17:17:26 +0900

    2021.10.11 유재헌 : 값 타입의 비교.

--- src/main/java/com/jpabasic/jpashop/domain2/Address.java
+++ src/main/java/com/jpabasic/jpashop/domain2/Address.java
@@ -1,5 +1,7 @@
 package com.jpabasic.jpashop.domain2;
 
+import java.util.Objects;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
@@ -41,6 +43,21 @@ public class Address {
 
 	private void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass()) return false;
+		Address address = (Address) o;
+		return Objects.equals(city , address.city) &&
+				Objects.equals(street, address.street) &&
+				Objects.equals(zipcode, address.zipcode);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, street, zipcode);
 	}
 
 }
